3c82a366a7065bc025ee36dba1e920b18a62e877
skylock <emanuil.copil@gmail.com>
2020-12-07 12:54:16 +0200

    add database connection timeout

--- config/database.js
+++ config/database.js
@@ -5,14 +5,17 @@ const db = {
     port: process.env.MONGO_PORT || 27017
 };
 
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000 // Keep trying to send operations for 5 seconds
+};
+
 const init = async () => {
     /* MONGOOSE SETUP */
     const connectionString = `mongodb://${db.host}:${db.port}/musicStore`;
 
-    await mongoose.connect(connectionString, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).catch(err => {
+    await mongoose.connect(connectionString,options).catch(err => {
         console.log(`Error connecting to mongo ${connectionString}`);
     });
 };
