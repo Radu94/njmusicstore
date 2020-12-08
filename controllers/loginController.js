22f6feadcd358a19abf0e8f510d749e0752c7547
dcocioab <dcocioab@cisco.com>
2020-12-08 20:35:13 +0200

    connect to DB and format code

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,42 +1,51 @@
-var bodyParser=require('body-parser');
-module.exports=function(app){
-/* MONGOOSE SETUP */
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://username:password@databaseserver:port/databasename',{ useNewUrlParser: true});
-/*Replace the above connection string with the actual connection string of your MongoDB database*/
-const Schema = mongoose.Schema;
-const UserDetail = new Schema({
-      username: String,
-      password: String
+var bodyParser = require('body-parser');
+module.exports = function (app) {
+  /* MONGOOSE SETUP */
+  const mongoose = require('mongoose');
+  const db_uri = 'mongodb+srv://test_user:test_pass@claudiacocioaba.ilhjc.mongodb.net/musicstore?retryWrites=true';
+  mongoose.connect(db_uri)
+    .then(result => {
+      console.log("connected!");
+    })
+    .catch(err => {
+      console.log(err);
     });
-const UserDetails = mongoose.model('userInfo', UserDetail, 'userInfo');
-/*  PASSPORT SETUP  START*/
-const passport = require('passport');
-app.use(passport.initialize());
-app.use(passport.session());
-app.get('/success', (req, res) => res.render('index',{username:req.query.username,title:'Home'}));
-app.get('/error', (req, res) => res.render('login',{username:'',title:'Login',errormessage:'An error occured while logging in. Please check your username and password!'}));
-passport.serializeUser(function(user, cb) {
-  cb(null, user.id);
-});
-passport.deserializeUser(function(id, cb) {
-  User.findById(id, function(err, user) {
-    cb(err, user);
+  /*Replace the above connection string with the actual connection string of your MongoDB database*/
+
+  const Schema = mongoose.Schema;
+  const UserDetail = new Schema({
+    username: String,
+    password: String
   });
-});
-/* PASSPORT SETUP STOP */
-    app.post('/', 
+  const UserDetails = mongoose.model('userInfo', UserDetail, 'userInfo');
+
+  /*  PASSPORT SETUP  START*/
+  const passport = require('passport');
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.get('/success', (req, res) => res.render('index', { username: req.query.username, title: 'Home' }));
+  app.get('/error', (req, res) => res.render('login', { username: '', title: 'Login', errormessage: 'An error occured while logging in. Please check your username and password!' }));
+  passport.serializeUser(function (user, cb) {
+    cb(null, user.id);
+  });
+  passport.deserializeUser(function (id, cb) {
+    User.findById(id, function (err, user) {
+      cb(err, user);
+    });
+  });
+  /* PASSPORT SETUP STOP */
+  app.post('/',
     passport.authenticate('local', { failureRedirect: '/error' }),
-    function(req, res) {        
-        res.redirect('/home',{username:req.user.username});
-  });
-/* PASSPORT LOCAL AUTHENTICATION */
-const LocalStrategy = require('passport-local').Strategy;
-passport.use(new LocalStrategy(
-  function(username, password, done) {
+    function (req, res) {
+      res.redirect('/home', { username: req.user.username });
+    });
+  /* PASSPORT LOCAL AUTHENTICATION */
+  const LocalStrategy = require('passport-local').Strategy;
+  passport.use(new LocalStrategy(
+    function (username, password, done) {
       UserDetails.findOne({
-        username: username 
-      }, function(err, user) {
+        username: username
+      }, function (err, user) {
         if (err) {
           return done(err);
         }
@@ -49,24 +58,24 @@ passport.use(new LocalStrategy(
         }
         return done(null, user);
       });
-  }
-));
-app.get('/',function(req, res) {
-    res.render('index', { username : '' , title:'Home'});
+    }
+  ));
+  app.get('/', function (req, res) {
+    res.render('index', { username: '', title: 'Home' });
   });
-  app.get('/index',function(req, res) {
-    res.render('index', { username : '' , title:'Home'});
+  app.get('/index', function (req, res) {
+    res.render('index', { username: '', title: 'Home' });
   });
   // Go to login page
-app.get('/login',function(req, res) {
-    res.render('login',{username : '' ,title: 'Login',errormessage:''});
-}); 
-app.post('/login', 
-  passport.authenticate('local', { failureRedirect: '/error' }),
-  function(req, res) {
-    res.redirect('/success?username='+req.user.username);
+  app.get('/login', function (req, res) {
+    res.render('login', { username: '', title: 'Login', errormessage: '' });
   });
-  app.get('/logout', function(req, res){
+  app.post('/login',
+    passport.authenticate('local', { failureRedirect: '/error' }),
+    function (req, res) {
+      res.redirect('/success?username=' + req.user.username);
+    });
+  app.get('/logout', function (req, res) {
     req.logout();
     res.redirect('/');
   });
