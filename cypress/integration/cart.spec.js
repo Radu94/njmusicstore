63b103d9de42c400b3b239b252c916fb21e0ad52
skylock <emanuil.copil@gmail.com>
2020-12-09 10:28:10 +0200

    update cart collection name

--- cypress/integration/cart.spec.js
+++ cypress/integration/cart.spec.js
@@ -8,7 +8,7 @@ describe('Shopping cart for', () => {
 
         it('should have two items', () => {
             cy.get('[data-cy=cart-item]').should('have.length', 2);
-            cy.get('[data-cy=trackname]').eq(0).should('have.text', 'A single Rose');
+            cy.get('[data-cy=trackname]').eq(0).should('have.text', 'A Single Rose');
             cy.get('[data-cy=trackname]').eq(1).should('have.text', 'Summer Rain');
             cy.get('[data-cy=grandtotal]').should('have.text', 1.98);
         });
