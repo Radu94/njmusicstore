f3c9e5daf91304c26902d9c93761bf0a7ed13b2c
Vlad Petrenciuc <vpetrenciuc@3pillarglobal.com>
2020-12-15 13:53:59 +0200

    Refactoring

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-var express=require('express');
-var expressLayouts = require('express-ejs-layouts');
-var app=express();
+const express=require('express');
+const expressLayouts = require('express-ejs-layouts');
+const app=express();
 app.set('view engine','ejs');
 app.use(expressLayouts);
 app.use(express.static('./public'));
 
 // dotenv
-var dotenv = require('dotenv');
+const dotenv = require('dotenv');
 dotenv.config();
 
 //passport
-var passport = require('passport');
+const passport = require('./config/passport')
 app.use(passport.initialize());
 app.use(passport.session());
-passport = require('./config/passport');
 
 // Routes
 const routes = require('./routes/routes');
 app.use(routes);
 
-var trackController=require('./controllers/trackController');
-var bodyParser=require('body-parser');
+const trackController=require('./controllers/trackController');
+const bodyParser=require('body-parser');
 
 app.use(bodyParser.urlencoded({extended:true}));
 trackController(app);
 
 //mongodb connection
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://' + process.env.MONGOUSER + ':' + process.env.MONGOPASSWORD + '@cluster0.jlrkv.mongodb.net/'+process.env.MONGODBNAME+'?retryWrites=true&w=majority', { useNewUrlParser: true })
 app.listen(4000);
 console.log('server listening');
